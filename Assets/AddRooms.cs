a0179db6b684a0de57b079cad9f00877e7355c3d
wlots <nicolassilva.ti@gmail.com>
2016-04-01 10:06:50 -0300

    New Rooms
    Adicionadas novas rooms que serao tomadas como padrão

---
+++ Assets/AddRooms.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddRooms : MonoBehaviour {
+
+
+    //[SerializeField] Transform roomStart;
+
+    public GameObject roomStart;
+
+    public GameObject[] rooms;
+    
+    public int nOfRooms;
+
+    private Room refRoom;
+    private Room roomToAdd;
+
+    // Use this for initialization
+    void Start () {
+
+        int r = (int)Mathf.Round((rooms.Length - 1) * Random.value);        
+        refRoom = roomStart.GetComponent<Room>();        
+        roomToAdd = rooms[r].GetComponent<Room>();
+        refRoom = (Room)addRoom(refRoom, roomToAdd);
+
+        for (int i=0; i<nOfRooms-1; i++)
+        {
+            r = (int)Mathf.Round((rooms.Length - 1) * Random.value);
+            roomToAdd = rooms[r].GetComponent<Room>();
+            refRoom = (Room)addRoom(refRoom, roomToAdd);
+        }        
+    }
+
+
+    public Object addRoom(Room refRoom, Room roomToAdd)
+    {
+        //Object[] obj = new Object(refRoom.nRightDoor);
+        if (refRoom.nRightDoor > 0)
+        {
+            //for (int j=0; j<refRoom.nLeftDoor; j++)
+            //{
+            Debug.Log(refRoom.nRightDoor);
+                for (int i = 0; i < refRoom.nRightDoor; i++)
+                {
+                    Vector3 posToAdd = refRoom.transform.position;
+                    posToAdd.x = posToAdd.x + refRoom.rightDoorLocation[i].x + roomToAdd.addOnLeft[0].x;
+                    posToAdd.y = posToAdd.y + refRoom.rightDoorLocation[i].y + +roomToAdd.addOnLeft[0].y;
+
+                    //Debug.Log("RefRoom x=" + refRoom.transform.position.x + " / Ref Room Right dor x=" + refRoom.rightDoorLocation[i].x + " / Room to add left x=" + roomToAdd.addOnLeft[0].x);
+                    //Debug.Log(posToAdd.x);                
+
+                    //obj[i] = Instantiate(roomToAdd, posToAdd, refRoom.transform.rotation);
+                }
+            //}            
+        }
+
+        //return obj;
+        return null;
+    }
+}
---
+++ Assets/Classes/Room.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour
+{
+    public int nLeftDoor;
+    public int nRightDoor;
+    public int nUpDoor;
+    public int nDownDoor;
+
+    public Vector2[] leftDoorLocation;
+    public Vector2[] rightDoorLocation;
+    public Vector2[] upDoorLocation;
+    public Vector2[] downDoorLocation;
+
+    public Vector2[] addOnLeft;
+    public Vector2[] addOnRight;
+    public Vector2[] addOnUp;
+    public Vector2[] addOnDown;
+
+    //Se a Room for Referencia (ou seja, vai ter uma Room sendo adicionada a ela), os campos necessarios
+    //sao a localização das portas.
+
+    //Se a Room vai ser adicionada a uma referencia, os campos necessarios sao os campos addOnX.
+
+    //--
+
+    //Pelo modo como os GameObjects de cada Room foram criados, para uma Room ser adicionada à:
+    //Esquerda:
+    //addOnLeft.x = leftDoorLocation.x + 1;
+    //addOnLeft.y = -leftDoorLocation.y;
+
+    //Direita:
+    //addOnRight.x = -(rightDoorLocation.x + 1);
+    //addOnRight.y = -rightDoorLocation.y;
+
+}
